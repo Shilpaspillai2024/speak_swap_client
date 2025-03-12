c53b297084e375a22a56bb58b66159b92455a5b6
Shilpaspillai2024 <shilpaspillaitvla@gmail.com>
2025-03-12 15:23:30 +0530

    fix(some minor changes)

--- src/app/classRoom/[bookingId]/page.tsx
+++ src/app/classRoom/[bookingId]/page.tsx
@@ -5,7 +5,7 @@ import { useParams } from "next/navigation";
 import { useBookingStore } from "@/store/bookingStore";
 import socketStore from "@/store/socketStore";
 import { completeSession } from "@/services/tutorApi";
-import ClassroomProtectedRoute from "@/HOC/ClassroomProtectedRoute";
+//import ClassroomProtectedRoute from "@/HOC/ClassroomProtectedRoute";
 import {
     Mic, MicOff, Video as VideoIcon, VideoOff,
     PhoneOff, Users, MessageCircle, X, Send,
@@ -719,4 +719,4 @@ const ClassRoom = () => {
   );
 };
 
-export default ClassroomProtectedRoute(ClassRoom);
+export default ClassRoom;
